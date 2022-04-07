ed541c1b9a1470b4d785c299a02858d98a9043e2
Samu <zamuelfernandes@gmail.com>
2022-04-07 12:05:19 -0300

    Arquitetando e Posicionando os elementos basicos

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,22 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  void decrement() {
+    print('Decrement');
+  }
+
+  void increment() {
+    print('Increment');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Color.fromARGB(255, 52, 168, 235),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          //CAMPO DE INFORMAÇÃO
           const Text(
             'Pode Entrar!!',
             style: TextStyle(
@@ -35,23 +44,65 @@ class HomePage extends StatelessWidget {
             ),
             textAlign: TextAlign.center,
           ),
-          const Text(
-            '11',
-            style: TextStyle(
-              fontSize: 100,
-              color: Colors.white,
+
+          // CAMPO DO VALOR
+          const Padding(
+            padding: EdgeInsets.all(70),
+            child: Text(
+              '11',
+              style: TextStyle(
+                fontSize: 100,
+                color: Colors.white,
+              ),
             ),
           ),
+
+          //ROW COM OS BOTÕES
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              FloatingActionButton(
-                onPressed: () {},
-                child: const Text('Saiu'),
+              //BOTÃO DE SAIU
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: const Size(100, 100),
+                  primary: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(color: Colors.black, width: 3),
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                ),
+                onPressed: decrement,
+                child: const Text(
+                  'Saiu',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
               ),
-              FloatingActionButton(
-                onPressed: () {},
-                child: const Text('Entrou'),
+              const SizedBox(width: 32),
+              //BOTÃO DE ENTROU
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: const Size(100, 100),
+                  primary: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(color: Colors.black, width: 3),
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                ),
+                onPressed: increment,
+                child: const Text(
+                  'Entrou',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
               ),
             ],
           )
